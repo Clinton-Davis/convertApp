98a95be866480466c6433939f9b0c327f3af4dac
Clinton-Dais <clintongadavis@gmail.com>
2022-02-14 21:03:43 +0000

    added loginc to temp and added Pinia stores

---
+++ src/store/useSelectOptions.js
@@ -0,0 +1,52 @@
+import { defineStore } from "pinia";
+
+export const useSelctOptionsStore = defineStore("temp", {
+    state: ()=> ({
+        tempScales: ['Fahrenheit', 'Celsius', 'Kelvin'],
+        userInput: null,
+        outPut: null,
+    }),
+    getters: {
+        getScales: (state) => {
+            state.tempScales
+        },
+        getOutPut: (state) => {
+            state.outPut
+        }
+    },
+    actions: {
+        reset() {
+            this.userInput = null;
+        },
+        CtoF(){
+            //(0°C × 9/5) + 32 = 32°F
+            let Input = this.userInput.value
+            this.outPut = (Input * 9/5) + 32
+        },
+        CtoK(){
+            // 0°C + 273.15 
+            let Input = this.userInput.value
+            this.outPut = Input + 273.15
+        },
+        FtoC(){
+            // (0°F − 32) × 5/9 = 
+            let Input = this.userInput.value
+            this.outPut = (Input - 32) * 5/9
+        },
+        FtoK(){
+           // (0°F − 32) × 5/9 + 273.15
+            let Input = this.userInput.value
+            this.outPut = (Input - 32) * 5/9 + 273.15
+        },
+        KtoC(){
+           // 0K − 273.15
+            let Input = this.userInput.value
+            this.outPut = Input - 273.15
+        },
+        KtoF(){
+           //(0K − 273.15) × 9/5 + 32 
+            let Input = this.userInput.value
+            this.outPut = (Input - 273.15) * 9/5 + 32
+        },
+    }
+})
