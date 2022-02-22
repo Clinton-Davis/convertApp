5be430099ff20d155bce590089570baf62582c66
Clitnon-Davis <clintongadavis@gmail.com>
2022-02-22 19:38:31 +0000

    added converstion to weightLogics

--- src/scripts/logics.js
+++ src/scripts/logics.js
@@ -1,89 +1,194 @@
 export const tempLogic = (amount, from, to) => {
     if (from === "Fahrenheit" && to === "Celsius") {
         let c = ((amount - 32) * 5) / 9;
-        return `${c.toFixed(2)} °C`;
+        return `${c} °C`;
     } else if (from === "Celsius" && to === "Fahrenheit") {
         let c = (amount * 9) / 5 + 32;
-        return `${c.toFixed(2)} °F`;
+        return `${c} °F`;
     } else if (from === "Celsius" && to === "Kelvin") {
         let c = amount + 273.15;
-        return `${c.toFixed(2)} K`;
+        return `${c} K`;
     } else if (from === "Kelvin" && to === "Celsius") {
         let c = amount - 273.15;
-        return `${c.toFixed(2)} °C`;
+        return `${c} °C`;
     } else if (from === "Kelvin" && to === "Fahrenheit") {
         let c = ((amount - 276.15) * 9) / 5 + 32;
-        return `${c.toFixed(2)} °F`;
+        return `${c} °F`;
     } else if (from === "Fahrenheit" && to === "Kelvin") {
         let c = ((amount - 32) * 5) / 9 + 273.15;
-        return `${c.toFixed(2)} K`;
+        return `${c} K`;
     }
 };
-["Tonne", "Kilogram", "Gram", "Milligram", "US Ton", "Stone", "Pound", "Ounce"];
+
 export const weightLogic = () => {
-    if (from === "Tonne" && to === "Kilogram") {
+    if (from === "Metric Ton" && to === "Kilogram") {
         let c = amount * 1000;
-        return `${Math.round(c)} Kg`;
-    } else if (from === "Tonne" && to === "Gram") {
+        return `${c} Kg`;
+    } else if (from === "Metric Ton" && to === "Gram") {
         let c = amount * 1000000;
-        return `${Math.round(c)} g`;
-    } else if (from === "Tonne" && to === "Milligram") {
+        return `${c} g`;
+    } else if (from === "Metric Ton" && to === "Milligram") {
         let c = amount * 1000000000;
-        return `${Math.round(c)} mg`;
-    } else if (from === "Tonne" && to === "US Ton") {
+        return `${c} mg`;
+    } else if (from === "Metric Ton" && to === "Imperial Ton") {
         let c = amount / 1.01604691;
-        return `${c.toFixed(2)} US Ton`;
-    } else if (from === "Tonne" && to === "Stone") {
+        return `${c} T`;
+    } else if (from === "Metric Ton" && to === "Stone") {
         let c = amount * 157.473045;
-        return `${c.toFixed(2)} Stone`;
-    } else if (from === "Tonne" && to === "Pound") {
+        return `${c} st`;
+    } else if (from === "Metric Ton" && to === "Pound") {
         let c = amount * 2204.62262;
-        return `${c.toFixed(2)} Pound`;
-    } else if (from === "Tonne" && to === "Ounce") {
+        return `${c} lb`;
+    } else if (from === "Metric Ton" && to === "Ounce") {
         let c = amount * 35273.962;
-        return `${c.toFixed(2)} oz`;
-    } else if (from === "Kilogram" && to === "Tonne") {
+        return `${c} oz`;
+    } else if (from === "Kilogram" && to === "Metric Ton") {
         let c = amount / 1000;
-        return `${Math.round(c)} Tonne`;
+        return `${c} t`;
     } else if (from === "Kilogram" && to === "Gram") {
         let c = amount * 1000;
-        return `${Math.round(c)} g`;
+        return `${c} g`;
     } else if (from === "Kilogram" && to === "Milligram") {
         let c = amount * 1000000;
-        return `${Math.round(c)} mg`;
-    } else if (from === "Kilogram" && to === "US Ton") {
+        return `${c} mg`;
+    } else if (from === "Kilogram" && to === "Imperial Ton") {
         let c = amount / 1016.04691;
-        return `${c.toFixed(2)} US Ton`;
+        return `${c} T`;
     } else if (from === "Kilogram" && to === "Stone") {
         let c = amount / 6.35029317;
-        return `${Math.round(c)} Stone`;
+        return `${c} st`;
     } else if (from === "Kilogram" && to === "Pound") {
         let c = amount * 2.20462262;
-        return `${c.toFixed(2)}K`;
+        return `${c}K`;
     } else if (from === "Kilogram" && to === "Ounce") {
         let c = amount * 35.273962;
-        return `${c.toFixed(2)} oz`;
-    } else if (from === "Gram" && to === "Tonne") {
+        return `${c} oz`;
+    } else if (from === "Gram" && to === "Metric Ton") {
         let c = amount * 1000;
-        return `${Math.round(c)} g`;
+        return `${c} g`;
     } else if (from === "Gram" && to === "Kilogram") {
         let c = amount * 1000000;
-        return `${Math.round(c)} mg`;
+        return `${c} mg`;
     } else if (from === "Gram" && to === "Milligram") {
         let c = amount * 1000;
-        return `${c.toFixed(2)} mg`;
-    } else if (from === "Gram" && to === "US Ton") {
+        return `${c} mg`;
+    } else if (from === "Gram" && to === "Imperial Ton") {
         let c = amount / 9.8421e-7;
-        return `${Math.round(c)} US Ton`;
+        return `${c} T`;
     } else if (from === "Gram" && to === "Stone") {
         let c = amount / 6350.29317;
-        return `${c.toFixed(2)}K`;
+        return `${c}K`;
     } else if (from === "Gram" && to === "Pound") {
         let c = amount / 453.59237;
-        return `${c.toFixed(2)} Pound`;
+        return `${c} lb`;
     } else if (from === "Gram" && to === "Ounce") {
         let c = amount / 28.3495231;
-        return `${c.toFixed(2)} oz`;
+        return `${c} oz`;
+    } else if (from === "Milligram" && to === "Metric Ton") {
+        let c = amount / 1.0e9;
+        return `${c} mg`;
+    } else if (from === "Milligram" && to === "Kilogram") {
+        let c = amount / 1000000;
+        return `${c} kg`;
+    } else if (from === "Milligram" && to === "Gram") {
+        let c = amount / 1000;
+        return `${c} g`;
+    } else if (from === "Milligram" && to === "Imperial Ton") {
+        let c = amount / 1.016e9;
+        return `${c} T`;
+    } else if (from === "Milligram" && to === "Stone") {
+        let c = amount / 6.3503e6;
+        return `${c}K`;
+    } else if (from === "Milligram" && to === "Pound") {
+        let c = amount / 453592.37;
+        return `${c} lb`;
+    } else if (from === "Milligram" && to === "Ounce") {
+        let c = amount / 28349.5231;
+        return `${c} oz`;
+    } else if (from === "Imperial Ton" && to === "Metric Ton") {
+        let c = amount * 1.01604691;
+        return `${c} t`;
+    } else if (from === "Imperial Ton" && to === "Kilogram") {
+        let c = amount * 1016.04691;
+        return `${c} kg`;
+    } else if (from === "Imperial Ton" && to === "Gram") {
+        let c = amount * 1.016e6;
+        return `${c} g`;
+    } else if (from === "Imperial Ton" && to === "Milligram") {
+        let c = amount * 1.016e9;
+        return `${c} mg`;
+    } else if (from === "Imperial Ton" && to === "Stone") {
+        let c = amount / 160;
+        return `${c} st`;
+    } else if (from === "Imperial Ton" && to === "Pound") {
+        let c = amount * 2240;
+        return `${c} lb`;
+    } else if (from === "Imperial Ton" && to === "Ounce") {
+        let c = amount * 35840;
+        return `${c} oz`;
+    } else if (from === "Stone" && to === "Metric Ton") {
+        let c = amount / 157.473045;
+        return `${c} t`;
+    } else if (from === "Stone" && to === "Kilogram") {
+        let c = amount * 6.35029317;
+        return `${c} mg`;
+    } else if (from === "Stone" && to === "Gram") {
+        let c = amount * 6350.29317;
+        return `${c} g`;
+    } else if (from === "Stone" && to === "Milligram") {
+        let c = amount * 6.3503e6;
+        return `${c} T`;
+    } else if (from === "Stone" && to === "Imperial Ton") {
+        let c = amount / 160;
+        return `${c}K`;
+    } else if (from === "Stone" && to === "Pound") {
+        let c = amount * 14;
+        return `${c} lb`;
+    } else if (from === "Stone" && to === "Ounce") {
+        let c = amount * 224;
+        return `${c} oz`;
+    } else if (from === "Pound" && to === "Metric Ton") {
+        let c = amount / 2204.62262;
+        return `${c} t`;
+    } else if (from === "Pound" && to === "Kilogram") {
+        let c = amount / 2.20462262;
+        return `${c} kg`;
+    } else if (from === "Pound" && to === "Gram") {
+        let c = amount * 453.59237;
+        return `${c} g`;
+    } else if (from === "Pound" && to === "Milligram") {
+        let c = amount * 453592.37;
+        return `${c} mg`;
+    } else if (from === "Pound" && to === "Imperial Ton") {
+        let c = amount / 2240;
+        return `${c} T`;
+    } else if (from === "Pound" && to === "Stone") {
+        let c = amount / 14;
+        return `${c} st`;
+    } else if (from === "Pound" && to === "Ounce") {
+        let c = amount / 16;
+        return `${c} oz`;
+    } else if (from === "Ounce" && to === "Metric Ton") {
+        let c = amount / 35273.962;
+        return `${c} t`;
+    } else if (from === "Ounce" && to === "Kilogram") {
+        let c = amount / 35.273962;
+        return `${c} kg`;
+    } else if (from === "Ounce" && to === "Gram") {
+        let c = amount * 28.3495231;
+        return `${c} g`;
+    } else if (from === "Ounce" && to === "Milligram") {
+        let c = amount * 28349.5231;
+        return `${c} mg`;
+    } else if (from === "Ounce" && to === "Imperial Ton") {
+        let c = amount / 35840;
+        return `${c} T`;
+    } else if (from === "Ounce" && to === "Stone") {
+        let c = amount / 224;
+        return `${c} st`;
+    } else if (from === "Ounce" && to === "Pound") {
+        let c = amount / 16;
+        return `${c} lb`;
     }
 };
 //https://www.bing.com/search?q=convert+tonnes+to+pounds&qs=MT&pq=convert+tonne&sk=MT1&sc=8-13&cvid=CACDD178D70142DA88F300ADDCFEDB1A&FORM=QBRE&sp=2
--- src/store/useWeightOptions.js
+++ src/store/useWeightOptions.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useWeightOptionsStore = defineStore("weight", {
     state: () => ({
-        weightScales: ["Tonne", "Kilogram", "Gram", "Milligram", "US Ton", "Stone", "Pound", "Ounce"],
+        weightScales: ["Metric Ton", "Kilogram", "Gram", "Milligram", "Imperial Ton", "Stone", "Pound", "Ounce"],
         userInput: [],
         outPut: null,
     }),
@@ -12,75 +12,6 @@ export const useWeightOptionsStore = defineStore("weight", {
         },
         getOutPut: (state) => {
             return state.outPut;
-        },
-    },
-    //TODO Finish all the actions.
-    actions: {
-        reset() {
-            this.userInput = [];
-        },
-
-        TtoUST() {
-            this.userInput * 1.102;
-        },
-        USTtoT() {
-            this.userInput / 1.102;
-        },
-
-        TtoS() {
-            this.userInput * 157;
-        },
-        StoT() {
-            this.userInput / 157;
-        },
-
-        TtoKG() {
-            this.userInput * 1000;
-        },
-        KGtoT() {
-            this.userInput / 1000;
-        },
-
-        TtoP() {
-            this.userInput * 2205;
-        },
-        PtoT() {
-            this.userInput / 2205;
-        },
-
-        TtoO() {
-            this.userInput * 35274;
-        },
-        OtoT() {
-            this.userInput / 35274;
-        },
-
-        TtoG() {
-            this.userInput * 1000000;
-        },
-        GtoT() {
-            this.userInput / 1000000;
-        },
-
-        TtoMilG() {
-            this.userInput * 1000000000;
-        },
-        MilGtoT() {
-            this.userInput / 1000000000;
-        },
-
-        TtoMicoG() {
-            this.userInput * 1000000000000;
-        },
-        MicoGtoT() {
-            this.userInput / 1000000000000;
-        },
-
-        KGtoG() {
-            this.userInput * 1000;
-        },
-        GtoKG() {
-            this.userInput / 1000;
         },
     },
 });
